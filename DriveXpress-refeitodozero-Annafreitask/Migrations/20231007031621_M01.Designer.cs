19cfde8b6ab36481109a383e88d5c6d3509c1164
Anna Freitas <102244252+AnnaFreitask@users.noreply.github.com>
2023-10-07 01:05:00 -0300

    atualizando 2

--- DriveXpress-refeitodozero-Annafreitask/Migrations/20231007031621_M01.Designer.cs
+++ DriveXpress-refeitodozero-Annafreitask/Migrations/20231007031621_M01.Designer.cs
@@ -4,6 +4,7 @@ using DriveXpress_refeitodozero_Annafreitask.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DriveXpress_refeitodozero_Annafreitask.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231007031621_M01")]
+    partial class M01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
